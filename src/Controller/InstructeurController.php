8598ccd09c0130452dc1c64ce46eebd7bddfa750
Gijs de Lange <gijs.de.lange@live.nl>
2019-12-12 10:22:22 +0100

    instructor crud

--- src/Controller/InstructeurController.php
+++ src/Controller/InstructeurController.php
@@ -4,6 +4,7 @@
 namespace App\Controller;
 
 
+use App\Entity\Lesson;
 use App\Form\LesType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -21,10 +22,11 @@ class InstructeurController extends AbstractController
     }
 
     /**
-     * @Route("/instr/plan_les", name="instr_plan_les")
+     * @Route("/instr/les_plan", name="instr_les_plan")
      */
     public function instrPlanLes(Request $request)
     {
+        $this->denyAccessUnlessGranted('ROLE_INSTR');
         $form = $this->createForm(LesType::class);
 
         $form->handleRequest($request);
@@ -37,9 +39,60 @@ class InstructeurController extends AbstractController
             $em->flush();
             $this->addFlash('success', 'les toegevoegd.');
 
-            return $this->redirectToRoute('instr_home');
+            return $this->redirectToRoute('instr_les_overzicht');
         }
 
-        return $this->render('instructeur/plan-les.html.twig', ['lesForm' => $form->createView()]);
+        return $this->render('instructeur/les-plan.html.twig', ['lesForm' => $form->createView()]);
     }
+
+    /**
+     * @Route("/instr/les_overzicht", name="instr_les_overzicht")
+     */
+    public function instrLesOverzicht(){
+        $this->denyAccessUnlessGranted('ROLE_INSTR');
+        $lessons = $this->getDoctrine()->getRepository(Lesson::class)->findAll();
+
+        return $this->render('instructeur/les-overzicht.html.twig', ["lessons"=>$lessons]);
+
+    }
+
+    /**
+     * @Route("/instr/les_wijzig/{id}", name="instr_les_wijzig")
+     */
+    public function instrLesWijzig(Request $request, $id){
+        $this->denyAccessUnlessGranted('ROLE_INSTR');
+        $lesson = $this->getDoctrine()->getRepository(Lesson::class)->find($id);
+        $form = $this->createForm(LesType::class, $lesson);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+
+            $training = $form->getData();
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($training);
+            $em->flush();
+            $this->addFlash('success', 'les is gewijzigd.');
+
+            return $this->redirectToRoute('instr_les_overzicht');
+        }
+
+        return $this->render('instructeur/les-plan.html.twig', ['lesForm' => $form->createView()]);
+    }
+
+    /**
+     * @Route("/instr/les_verwijder/{id}", name="instr_les_verwijder")
+     */
+    public function instrLesVerwijder($id){
+        $this->denyAccessUnlessGranted('ROLE_INSTR');
+        $em = $this->getDoctrine()->getManager();
+        $lesson = $em->getRepository(Lesson::class)->find($id);
+
+        $em->remove($lesson);
+        $em->flush();
+        $this->addFlash('success', 'Training verwijderd.');
+
+        return $this->redirectToRoute('instr_les_overzicht');
+    }
+
 }
--- src/Entity/Lesson.php
+++ src/Entity/Lesson.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\LessonRepository")
@@ -30,11 +31,13 @@ class Lesson
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank(message="vul een locatie in")
      */
     private $location;
 
     /**
      * @ORM\Column(type="integer")
+     * @Assert\NotBlank(message="vul een aantal in")
      */
     private $max_persons;
 
